d760e001cd96233bfdba849f065374ccf54c21e8
root <root@ip-172-31-43-23.eu-north-1.compute.internal>
2025-03-06 05:19:07 +0000

    hello chatur

---
+++ hello_chatur.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "chatur: Adarniya sabhapati mahodaya"
+
+echo "chatur: aaj hum sub mil kr chamatkar karyege"
+
+echo "rancho: ratta marega toh aisha hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+echo "raju: rancho ithna hi tuje toh ek folder banake dikhana"
+
+mkdir -p rancho
+
+echo "rancho: ye leh, bana diya, ro mat ab"
+
+echo "farhan: viurus ne beizaati kar di, bola farhanitrate"
+
+echo "farhan: abba nhi manege" > farhanitrate.txt
